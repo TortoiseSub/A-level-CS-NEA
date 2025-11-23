a4180dc489a115d5223aaa55384ec16183077512
TortoiseSub <tommybryanburn@gmail.com>
2025-11-23 18:11:34 +0000

    modified:   NEA/public/saveFileHandling.js

--- NEA/public/saveFileHandling.js
+++ NEA/public/saveFileHandling.js
@@ -27,6 +27,9 @@ function receiveWriteData(err){
 }
 
 //Encryption
+
+let initialState = 
+
 function getSavingData(){
     saveData = {
         jumpHacked : jumphacked,
@@ -39,19 +42,62 @@ function getSavingData(){
 function generateSalt(){
 
 }
-function generateHMAC(data,key){
+function generateHMAC(data,initialState){
 
 }
-function cipherData(data,key){
-//Test data : TestData123!@#_Example$%^&*(2025)-+=[]{}|;:'",.<>?/`~
+function cipherData(data,initialState){
+    //Test data : TestData123!@#_Example$%^&*(2025)-+=[]{}|;:'",.<>?/`~
+    let plainTextCodes = []
+    let encryptedTextCodes = []
+    let currentState = initialState
+    let key
+    //1. Convert data to bit codes for each character
+    ////Repeat start
+    //2. Generate first key from state
+    //3. Encode bit code using plaintext bit code XOR key 
+    //4. Hash Encrypted bitcode and key
+    //5. Save hash as new state
+    //6. Generate second key from state second position
+    ////7. Repeat for all bits
 
+    //1.
+    for(i = 0; i < data.length; i++){
+        plainTextCodes = [].push(data.charCodeAt(i))
+    }
 
+    for(i = 0; i < plainTextCodes.length; i++){
+        //2. 
+        key = currentState.slice(i*8, i*8 +8)
+        //3. 
+        encryptedTextCodes.push(maskXOR(plainTextCodes[i],key))
+        //4. 5.
+        currentState = hash(encryptedTextCodes[i],key)
+        
+    }
+    
+}
 
+function encryptData(data, initialState){
 
+}
 
-
-
-
-
-
+function maskXOR(value1, value2){
+    let XORValue = []
+    if(value1.length == value2.length){
+        for(i = 0; i < value1.length ; i++){
+            let iterationValue = -1
+            if(value1[i] == value2[i]){
+                iterationValue = 0
+            }
+            if(value1[i] == value2[i]){
+                iterationValue = 1
+            }
+            XORValue.push(iterationValue)
+        }
+        XORValue = XORValue.toString()
+    }
+    else{
+        console.log('Values are not the same length')
+    }
+    return XORValue
 }
